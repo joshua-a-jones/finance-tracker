1b15620a834a0a5841a50a3f25ea88d737701082
Joshua Jones <joshua.desjones@gmail.com>
2022-05-12 08:57:51 -0500

    Added react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import React from "react";
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import { Router } from "express";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<div>Home</div>} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
