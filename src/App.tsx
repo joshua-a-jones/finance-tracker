36f659a728ea3ac5db03229f2a8db9ebdca052da
Joshua Jones <joshua.desjones@gmail.com>
2022-05-12 09:50:55 -0500

    added basic pages and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import React from "react";
-import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import { Router } from "express";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<div>Home</div>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </div>
   );
