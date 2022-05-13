cae8b4395f51619c643cb4df199bd5685ea50bdf
Joshua Jones <joshua.desjones@gmail.com>
2022-05-13 07:46:00 -0500

    added registration form

--- src/pages/register/Register.tsx
+++ src/pages/register/Register.tsx
@@ -1,5 +1,75 @@
+import { FormEvent, useState } from "react";
+
+// styles
 import "./Register.css";
 
 export default function Register() {
-  return <div className="register">Register</div>;
+  const [email, setEmail] = useState("");
+  const [displayName, setDisplayName] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmationPassword, setConfirmationPassword] = useState("");
+  const [isEmailValid, setIsEmailValid] = useState(false);
+  const [isPasswordValid, setIsPasswordValid] = useState(false);
+
+  const handleFormSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
+  const handlEmailInputChange = (e: FormEvent<HTMLInputElement>) => {
+    setEmail(e.currentTarget.value);
+    setIsEmailValid(!e.currentTarget.validity.typeMismatch);
+  };
+
+  const handlPasswordInputChange = (e: FormEvent<HTMLInputElement>) => {
+    setPassword(e.currentTarget.value);
+    setIsPasswordValid(!e.currentTarget.validity.patternMismatch);
+  };
+
+  return (
+    <form className="registration-form" onSubmit={handleFormSubmit}>
+      <h2>Register</h2>
+      <label>
+        <span>Username:</span>
+        <input
+          type="text"
+          value={displayName}
+          onChange={(e) => setDisplayName(e.target.value)}
+          required
+        ></input>
+      </label>
+      <label>
+        <span>Email:</span>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => handlEmailInputChange(e)}
+          required
+        ></input>
+      </label>
+      <fieldset>
+        <legend>Choose a password</legend>
+        <label>
+          <span>Password:</span>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => handlPasswordInputChange(e)}
+            required
+          ></input>
+        </label>
+        <label>
+          <span>Confirm password:</span>
+          <input
+            type="password"
+            value={confirmationPassword}
+            onChange={(e) => setConfirmationPassword(e.target.value)}
+            required
+          ></input>
+        </label>
+      </fieldset>
+      <button className="button" type="submit">
+        Submit
+      </button>
+    </form>
+  );
 }
