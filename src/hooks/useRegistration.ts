7915db11e030c7cb1f4b867d5d98f6ff03ee7439
Joshua Jones <joshua.desjones@gmail.com>
2022-05-13 08:16:45 -0500

    added userbname update to registration hook

--- src/hooks/useRegistration.ts
+++ src/hooks/useRegistration.ts
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { auth } from "../firebase/config";
 import { createUserWithEmailAndPassword } from "@firebase/auth";
 import { getErrorMessage } from "../utils/errorHandling";
+import { updateProfile } from "firebase/auth";
 
 export const useRegistration = () => {
   const [error, setError] = useState<null | string>(null);
@@ -17,8 +18,14 @@ export const useRegistration = () => {
 
     try {
       const cred = await createUserWithEmailAndPassword(auth, email, password);
+
+      if (!cred) throw new Error("Unable to complete signup");
+
+      // add display name to user
+      await updateProfile(cred.user, { displayName });
+
       setIsPending(false);
-      const user = cred.user;
+      setError(null);
     } catch (error: unknown) {
       console.log(error);
       setIsPending(false);
