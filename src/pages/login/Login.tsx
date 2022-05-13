374f2208650fbdf2d56506b6385b6462dfd18d44
Joshua Jones <joshua.desjones@gmail.com>
2022-05-12 19:38:13 -0500

    created login form

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,5 +1,37 @@
+import { FormEvent, ReactEventHandler, useState } from "react";
+
+//styles
 import "./Login.css";
 
 export default function Login() {
-  return <div className="login">Login</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleFormSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+  return (
+    <form className="login-form" onSubmit={handleFormSubmit}>
+      <h2>Login</h2>
+      <label>
+        <span>Email:</span>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        ></input>
+      </label>
+      <label>
+        <span>Password:</span>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        ></input>
+      </label>
+      <button className="button" type="submit">
+        Sign In
+      </button>
+    </form>
+  );
 }
