48ca6d86ece85c0ac84915d75f110b0e6374007b
Joshua Jones <joshua.desjones@gmail.com>
2022-05-14 09:01:15 -0500

    wait to render app until auth ready

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,30 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
-import { Router } from "express";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
 import Navbar from "./components/navbar/Navbar";
+import { useAuthContext } from "./hooks/useAuthContext";
 
 function App() {
+  const { authIsReady, user } = useAuthContext();
   return (
     <div className="App">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
+      {authIsReady && (
+        <>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route
+              path="/login"
+              element={user ? <Navigate to="/" /> : <Login />}
+            />
+            <Route
+              path="/register"
+              element={user ? <Navigate to="/" /> : <Register />}
+            />
+          </Routes>
+        </>
+      )}
     </div>
   );
 }
