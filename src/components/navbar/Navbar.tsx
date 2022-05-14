541635344f82031cbdcb96040e2cf70a293e19b6
Joshua Jones <joshua.desjones@gmail.com>
2022-05-13 21:27:37 -0500

    add logout button to nav

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,16 +1,34 @@
 import { Link } from "react-router-dom";
+import { useLogout } from "../../hooks/useLogout";
+import { useAuthContext } from "../../hooks/useAuthContext";
+
+//styles
 import "./Navbar.css";
+
 export default function Navbar() {
+  const { user } = useAuthContext();
+  const { logoutUser } = useLogout();
   return (
     <nav className="navbar">
       <ul>
         <li className="title">myMoney</li>
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
-        <li>
-          <Link to="/register">Register</Link>
-        </li>
+        {!user && (
+          <>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/register">Register</Link>
+            </li>
+          </>
+        )}
+        {user && (
+          <li>
+            <button className="button" onClick={logoutUser}>
+              Logout
+            </button>
+          </li>
+        )}
       </ul>
     </nav>
   );
